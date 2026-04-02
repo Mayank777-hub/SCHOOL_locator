8251c39e12aedb8fcd6185dc0c66b40085fc7acf
MAYANK <mmkumar0622@gmail.com>
2026-04-02 23:26:39 +0530

    Fixed some bugs

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 5300;
+const PORT = process.env.PORT || 5300;
+const cors = require("cors");
 app.use(express.json());
+
+app.use(cors());
 
 const schoolRoutes = require("./routes/schoolroutes");
 app.use("/api", schoolRoutes);
